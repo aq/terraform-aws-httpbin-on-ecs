a6506c36fddfac9b12b1626d340d3b2945ff7052
Antoine Qu'hen <antoinequhen@gmail.com>
2021-01-05 07:48:51 +0100

    Add execution role to the task

--- 02-ecs.tf
+++ 02-ecs.tf
@@ -4,8 +4,34 @@ resource "aws_ecs_task_definition" "this" {
   requires_compatibilities = ["FARGATE"]
   cpu = 256
   memory = 512
+  execution_role_arn = aws_iam_role.httpbin_execution_role.arn
 
   network_mode = "awsvpc"
+}
+
+resource "aws_iam_role" "httpbin_execution_role" {
+  name = "httpbin-execution-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "httpbin_execution_role_policy_attachement" {
+  role       = aws_iam_role.httpbin_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 resource "aws_ecs_cluster" "this" {
